93f80e17ba464b6cd00a12f43fd191775266d116
Bhavesh Songara <bhaveshsongara143@gmail.com>
2020-11-11 16:28:17 +0530

    initial commit

--- stopwatch/src/main.js
+++ stopwatch/src/main.js
@@ -1,12 +1,113 @@
-document.querySelector("#startBtn").addEventListener("click", startTimer);
+let miliSec = 0;
+let sec = 0;
+let min = 0;
 
- function startTimer() {
-     let timeString = document.querySelector(".watch").textContent;
-     let timeArray = timeString.split(":");
-     let min = timeArray[0];
-     let sec = timeArray[1];
-     let miliSec = timeArray[2];
-    let time = miliSec + 1;
-    document.querySelector(".watch").textContent = time;
-    setTimeout(startTimer, 1000);
- }
+let displayMs = 0;
+let displaySec = 0;
+let displayMin = 0;
+
+let status = "stop";
+
+let interval = null;
+
+function startWatch() {
+    document.querySelector("#startBtn").classList.add("hidden");
+    document.querySelector("#pauseBtn").classList.remove("hidden");
+    document.querySelector("#lapBtn").classList.remove("hidden");
+    document.querySelector("#lapBtn").classList.add("active");
+    if(!document.querySelector("#resetBtn").classList.contains("hidden")) {
+        document.querySelector("#resetBtn").classList.add("hidden");
+    }
+    miliSec++
+    
+   if(miliSec / 100 == 1) {
+       miliSec = 0; 
+       sec++;
+       if(sec / 60 == 1) {
+           sec = 0;
+           min++;
+       }
+   }
+   if(miliSec < 10) {
+    displayMs = "0" + miliSec;
+}else {
+    displayMs = miliSec;
+}
+   if(sec < 10) {
+       displaySec = "0" + sec;
+   }else {
+       displaySec = sec;
+   }
+   if(min < 10) {
+       displayMin = "0" + min
+   }else {
+       displayMin = min;
+   }
+   document.querySelector(".watch").textContent = displayMin + ":" + displaySec + ":" + displayMs;
+}
+
+function stopWatch() {
+    document.querySelector("#pauseBtn").classList.add("hidden");
+    document.querySelector("#lapBtn").classList.add("hidden");
+    document.querySelector("#startBtn").classList.remove("hidden");
+    document.querySelector("#resetBtn").classList.remove("hidden");
+}
+
+function resetWatch() {
+    document.querySelector("#pauseBtn").classList.add("hidden");
+    document.querySelector("#resetBtn").classList.add("hidden");
+    document.querySelector("#lapBtn").classList.remove("active");
+    document.querySelector("#lapBtn").classList.remove("hidden");
+    miliSec = 0;
+    sec = 0;
+    min = 0;
+    document.querySelector(".watch").textContent = "00:00:00";
+    while(document.querySelector(".lap-data-container").firstChild) {
+        document.querySelector(".lap-data-container").removeChild(document.querySelector(".lap-data-container").firstChild);
+    }
+    // document.querySelector()
+}
+
+function lapWatch() {
+    if(document.querySelector("#lapBtn").classList.contains("active")) {
+        let lapDiv = document.createElement("div");
+        lapDiv.classList.add("lap");
+        let srNo = document.createElement("p");
+        if(document.querySelector(".lap-data-container").children.length > 0) {
+          let text =  document.querySelector(".lap-data-container").firstElementChild.firstElementChild.textContent;
+          srNo.textContent = Number(text) + 1;
+        }else {
+            srNo.textContent = "1"
+        }
+        let lapTime = document.createElement("p");
+        lapTime.textContent = document.querySelector(".watch").textContent;
+        lapTime.classList.add("gap");
+        
+        lapDiv.append(srNo);
+        lapDiv.append(lapTime);
+
+        document.querySelector(".lap-data-container").prepend(lapDiv);
+        document.querySelector(".lap-data-container").classList.remove("hidden");
+    }
+}
+
+function checkStatus() {
+    if(status == "stop") {
+        interval = window.setInterval(startWatch, 10);
+        status = "start"
+    }else {
+        window.clearInterval(interval)
+        status = "stop";
+        stopWatch();
+    }
+
+}
+
+
+document.querySelector("#startBtn").addEventListener("click", checkStatus);
+
+document.querySelector("#pauseBtn").addEventListener("click", checkStatus);
+
+document.querySelector("#resetBtn").addEventListener("click", resetWatch);
+
+document.querySelector("#lapBtn").addEventListener("click", lapWatch);
